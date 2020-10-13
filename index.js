000239b55f88152bdb7f104e4dde9546faf21b53
Kristopher Kaae <kskaae@gmail.com>
2020-10-13 08:26:10 -0700

    added new info

---
+++ index.js
@@ -0,0 +1,27 @@
+// array of questions for user
+const questions = [
+
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init();
+
+
+// * Title
+//   * Description
+//   * Table of Contents
+//   * Installation
+//   * Usage
+//   * License
+//   * Contributing
+//   * Tests
+//   * Questions
