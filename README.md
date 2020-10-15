0208eec1b05ae4cc9e547662c797d84fd06a0cb1
Kristopher Kaae <kskaae@gmail.com>
2020-10-14 19:00:21 -0700

    Added links

--- README.md
+++ README.md
@@ -1,59 +1,78 @@
 
-# 2
+# ReadME
 
 Simple overview of use/purpose.
+My goals included:
+creating a simple markdown ReadME for ease of use on future projects.
+Throughout this project, I specifically implemented the information 
+I have gathered from the previous course instruction. 
+ 
 
 ## Description
-
+*[GitHub](https://kskaae.github.io/README-Generator/.)
 An in-depth paragraph about your project and overview of use.
 
 ## Getting Started
-
+Log onto Github and download =>
 ### Dependencies
+  Windows 10
+  NPM install 
 
-* Describe any prerequisites, libraries, OS version, etc., needed before installing program.
-* ex. Windows 10
+### Install at :
+[Video Description](https://drive.google.com/file/d/173muS3flxEHb26IS_j8PtS6OBlB4Eo5e/view)
 
-### Installing
-
-* How/where to download your program
-* Any modifications needed to be made to files/folders
+* https://github.com/Kskaae/README-Generator
 
 ### Executing program
+* Download 
+* Modify 
+* Enjoy
 
-* How to run the program
-* Step-by-step bullets
+
 
 ## Help
-
-Any advise for common problems or issues.
-
+[Twitter @](https://twitter.com/KaaeTopher)
+No known issues 
 
 ## Authors
 
-Contributors names and contact info
-
-ex. Dominique Pizzie  
-ex. [@DomPizzie](https://twitter.com/dompizzie)
+Kristopher Kaae  
+[Twitter @](https://twitter.com/KaaeTopher)
 
 ## Version History
 
-* 0.2
-    * Various bug fixes and optimizations
-    * See [commit change]() or See [release history]()
 * 0.1
     * Initial Release
 
-## License
+## License 
 
-This project is licensed under the [NAME HERE] License - see the LICENSE.md file for details
+*MIT License:
+
+Copyright (c) [2020] [Kristopher S. Kaae]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+This project is licensed under the [MIT] License - see the LICENSE.md file for details
 
 ## Acknowledgments
 
-Inspiration, code snippets, etc.
+
+
+Inspiration, code snippets, etc.:
 * [awesome-readme](https://github.com/matiassingers/awesome-readme)
-* [PurpleBooth](https://gist.github.com/PurpleBooth/109311bb0361f32d87a2)
-* [dbader](https://github.com/dbader/readme-template)
-* [zenorocha](https://gist.github.com/zenorocha/4526327)
-* [fvcproductions](https://gist.github.com/fvcproductions/1bfc2d4aecb01a834b46)
         
--- index.js
+++ index.js
@@ -7,30 +7,20 @@ const util = require('util')
 const questions = [{
     name: "name",
     type: "input",
-    message: "what is your name?"
-
+    message: "what is your Repository name?"
 },
 {
-    name: "age",
-    type: "input",
-    message: "what is your age ?",
-},
-{
-    name: "repositoryName",
-    type: "input",
-    message: "What is the name of your Repository?",
+    name: "licenseInfo",
+    type: "list",
+    choices:["MIT","Apache", "GNU GPL"], 
+    message: "Are you using a license?"
 },
 {
     name: "repoInformation",
     type: "input",
     message: " what information would you like to include about your repository?"
 },
-{
-    name: "licenseInfo",
-    type: "list",
-    choices:["MIT","Apache", "ect","ect"], 
-    message: " what information would you like to include about your repository?"
-}
+
 ];
 // function to write README file
 function writeToFile(fileName, data) {
@@ -52,60 +42,78 @@ function init() {
 # ${response.name}
 
 Simple overview of use/purpose.
+My goals included:
+creating a simple markdown ReadME for ease of use on future projects.
+Throughout this project, I specifically implemented the information 
+I have gathered from the previous course instruction. 
+ 
 
 ## Description
-
+*[GitHub](https://kskaae.github.io/README-Generator/.)
 An in-depth paragraph about your project and overview of use.
 
 ## Getting Started
-
+Log onto Github and download =>
 ### Dependencies
+  Windows 10
+  NPM install 
 
-* Describe any prerequisites, libraries, OS version, etc., needed before installing program.
-* ex. Windows 10
+### Install at :
 
-### Installing
-
-* How/where to download your program
-* Any modifications needed to be made to files/folders
+* https://github.com/Kskaae/README-Generator
 
 ### Executing program
+* Download 
+* Modify 
+* Enjoy
 
-* How to run the program
-* Step-by-step bullets
+
 
 ## Help
-
-Any advise for common problems or issues.
-
+[Twitter @](https://twitter.com/KaaeTopher)
+No known issues 
 
 ## Authors
 
-Contributors names and contact info
-
-ex. Dominique Pizzie  
-ex. [@DomPizzie](https://twitter.com/dompizzie)
+Kristopher Kaae  
+[Twitter @](https://twitter.com/KaaeTopher)
 
 ## Version History
 
-* 0.2
-    * Various bug fixes and optimizations
-    * See [commit change]() or See [release history]()
 * 0.1
     * Initial Release
 
-## License
+## License 
 
+*MIT License:
+
+Copyright (c) [2020] [Kristopher S. Kaae]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 This project is licensed under the [${response.licenseInfo}] License - see the LICENSE.md file for details
 
 ## Acknowledgments
 
-Inspiration, code snippets, etc.
+
+
+Inspiration, code snippets, etc.:
 * [awesome-readme](https://github.com/matiassingers/awesome-readme)
-* [PurpleBooth](https://gist.github.com/PurpleBooth/109311bb0361f32d87a2)
-* [dbader](https://github.com/dbader/readme-template)
-* [zenorocha](https://gist.github.com/zenorocha/4526327)
-* [fvcproductions](https://gist.github.com/fvcproductions/1bfc2d4aecb01a834b46)
         `
         writeToFile("./readMe.md", text)
     })
@@ -113,15 +121,3 @@ Inspiration, code snippets, etc.
 
 // function call to initialize program
 init();
-
-// inquirer.prompt({
-//     ReadME: "Welcome to ReadME!",
-//     Description:"",
-//     Contents:"",
-//     Installation :"",
-//     Usage:"",
-//     License :"",
-//     Contributing:"",
-//     Tests :"",
-//     Questions:"",
-// });
